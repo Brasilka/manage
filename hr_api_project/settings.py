dd41260537a32eae90bf24573e60c6faef40cf7c
Brasilka <163729824+Brasilka@users.noreply.github.com>
2025-06-08 20:56:56 +0900

    Update settings.py

--- hr_api_project/settings.py
+++ hr_api_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=6946z$r6-*@0w_yyrk--^%f0lqy4%+_pd3(p7#5b6d)!(up!9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
