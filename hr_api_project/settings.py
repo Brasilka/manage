7a11c1e6427c3e884736e0cbbebd15a40312f2ab
Brasilka <iypredatoryi@mail.ru>
2025-06-08 20:55:12 +0900

    fix: allow all hosts temporarily for deployment

--- hr_api_project/settings.py
+++ hr_api_project/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = 'django-insecure-=6946z$r6-*@0w_yyrk--^%f0lqy4%+_pd3(p7#5b6d)!(up!9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    os.environ.get("RENDER_EXTERNAL_HOSTNAME", "localhost")
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
