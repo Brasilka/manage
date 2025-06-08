70f5ddbfe9eea5bbd7be5096e2f331ebe8be4825
Brasilka <163729824+Brasilka@users.noreply.github.com>
2025-06-08 21:08:01 +0900

    Update settings.py

--- hr_api_project/settings.py
+++ hr_api_project/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from datetime import timedelta
 
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
